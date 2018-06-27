20a19d9d7af138fe20353e00d64310fa4df0dbed
Alex Marchenko <alexvmarch@gmail.com>
2018-06-27 09:06:59 -0400

    Added creation tests for aerial objects

--- dronedirector/tests/test_aerial.py
+++ dronedirector/tests/test_aerial.py
@@ -5,6 +5,10 @@
 Test Aerial Objects
 #####################
 """
+import six
+import json
+import uuid
+import numpy as np
 from itertools import cycle
 from dronedirector.aerial import AerialObject, Drone, SinusoidalDrone
 
@@ -16,6 +20,31 @@ class CaliRedwood(AerialObject):
                                           latitude=cycle([37.8716]),
                                           longitude=cycle([-122.2727]))
 
+def test_simple_aerial():
+    """Test making a simple object on-the-fly."""
+    tree = AerialObject(altitude=cycle([100.0]),
+                        latitude=cycle([37.8716]),
+                        longitude=cycle([-122.2727]))
+    msg = tree.message()
+    assert isinstance(msg, six.string_types)
+    assert isinstance(json.loads(msg), dict)
+
+
 def test_subclassing_ao():
     """Test subclassing :class:`~dronedirector.aerial.AerialObject`."""
     tree = CaliRedwood()
+    msg = json.loads(tree.message())    # Tests for valid json
+    assert isinstance(msg, dict)
+    assert np.isclose(msg['altitude'], 100.0)
+
+
+def test_drone():
+    """Test basic drone creation."""
+    uid = uuid.uuid4()
+    drone = Drone(cycle([1000.0]), cycle([41.0]), region="New York", uid=uid,
+                  longitude=cycle(np.sin(np.arange(0, 2*np.pi, np.pi/360))))
+    assert drone.region == "New York"
+    assert drone.uid == uid
+    msg = json.loads(drone.message())
+    assert len(msg) == 6
+
